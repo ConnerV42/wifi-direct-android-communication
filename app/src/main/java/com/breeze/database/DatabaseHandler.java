e4a849ce7a5b0594158abe39155f4fbdd74523a7
jake stevens <jakestevens081@gmail.com>
2019-11-04 07:49:41 -0800

    Added fields for contact, added more methods for db access

--- app/src/main/java/com/breeze/database/DatabaseHandler.java
+++ app/src/main/java/com/breeze/database/DatabaseHandler.java
@@ -4,10 +4,18 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 import com.breeze.models.BrzContact;
 import com.breeze.models.BrzMessage;
 import com.breeze.models.BrzPreference;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
 
@@ -25,16 +33,16 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     }
     @Override
     public void onCreate(SQLiteDatabase db) {
-        final String INIT_CONTACT_TABLE = "CREATE TABLE IF NOT EXISTS Contacts ('id' INTEGER PRIMARY KEY, 'name' TEXT NOT NULL, 'alias' TEXT NOT NULL, 'signature' TEXT NOT NULL)";
+        final String INIT_CONTACT_TABLE = "CREATE TABLE IF NOT EXISTS Contacts ('id' INTEGER PRIMARY KEY, 'name' TEXT NOT NULL, 'alias' TEXT NOT NULL, 'signature' TEXT NOT NULL, 'lasttalkedto' DATE NOT NULL, 'blocked' BOOLEAN NOT NULL, 'friend' BOOLEAN NOT NULL)";
         db.execSQL(INIT_CONTACT_TABLE);
         final String INIT_MESSAGE_TABLE = "CREATE TABLE IF NOT EXISTS Messages ('id' INTEGER PRIMARY KEY, 'from' INTEGER NOT NULL, 'body' TEXT NOT NULL, 'datetime' TEXT NOT NULL, 'encryption' TEXT DEFAULT NULL, FOREIGN KEY ('from') REFERENCES Contacts('id'))";
         db.execSQL(INIT_MESSAGE_TABLE);
         final String INIT_PREFS_TABLE = "CREATE TABLE IF NOT EXISTS Preferences ('id' INTEGER PRIMARY KEY, 'name' TEXT NOT NULL, 'setting' TEXT NOT NULL)";
         db.execSQL(INIT_PREFS_TABLE);
-        System.out.println("tables created");
+        Log.i("DatabaseInfo", "Table Creations succeeded for Contacts, Messages, Preferences");
     }
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(@NotNull SQLiteDatabase db, int oldVersion, int newVersion) {
         //migration process if we want to upgrade in future.
         //For now, it'll drop all tables and redo onCreate
         final String DROP_CONTACT_TABLE = "DROP TABLE IF EXISTS Contact";
@@ -45,7 +53,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.execSQL(DROP_PREFS_TABLE);
         this.onCreate(db);
     }
-    public void addContact(BrzContact brzContact)
+    public void addContact(@org.jetbrains.annotations.NotNull BrzContact brzContact)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues vals = new ContentValues();
@@ -53,21 +61,24 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         vals.put("name", brzContact.getName());
         vals.put("alias", brzContact.getAlias());
         vals.put("signature", brzContact.getSignature());
+        vals.put("lasttalkedto", brzContact.getLastTalkedTo());
+        vals.put("friend", brzContact.isFriend());
+        vals.put("blocked", brzContact.isBlocked());
         db.insert(CONTACTS_TABLE_NAME, null, vals);
         db.close();
     }
-    public void addMessage(BrzMessage message)
+    public void addMessage(@NotNull BrzMessage message)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues vals = new ContentValues();
         vals.put("id", message.getId());
         vals.put("body", message.getBody());
-        vals.put("datetime", message.getDatetime().toString());
+        vals.put("datetime", message.getDatetime());
         vals.put("encryption", message.getEncryption());
         db.insert(MESSAGES_TABLE_NAME, null, vals);
         db.close();
     }
-    public void addPreference(BrzPreference preference)
+    public void addPreference(@NotNull BrzPreference preference)
     {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues vals = new ContentValues();
@@ -77,6 +88,37 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.insert(PREFERENCES_TABLE_NAME, null, vals);
         db.close();
     }
+    public void deleteOneContact(@NotNull BrzContact contact)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(CONTACTS_TABLE_NAME, "id = ?", new String[]{ String.valueOf(contact.getId())});
+        db.close();
+    }
+    public void deleteOneMessage(@NotNull BrzMessage message)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(MESSAGES_TABLE_NAME, "id = ?", new String[]{ String.valueOf(message.getId())});
+        db.close();
+    }
+    public void deleteOnePreference(@NotNull BrzPreference preference)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(PREFERENCES_TABLE_NAME, "id = ?", new String[]{ String.valueOf(preference.getId())});
+        db.close();
+    }
 
-
+    public int updateContact(BrzContact contact)
+    {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        return -1;
+    }
+    public int updateMessage(BrzMessage message)
+    {
+        return -1;
+    }
+    public int updatePreference(BrzPreference preference)
+    {
+        return -1;
+    }
 }
