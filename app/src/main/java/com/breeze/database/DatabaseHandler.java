6b561eff8a56b53e176206695f88638c74af6a70
jake <jstevens5@eagles.ewu.edi>
2019-11-28 21:54:24 -0800

    db methods -- nodes, messages, chats

--- app/src/main/java/com/breeze/database/DatabaseHandler.java
+++ app/src/main/java/com/breeze/database/DatabaseHandler.java
@@ -10,13 +10,17 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 
 import com.breeze.datatypes.BrzChat;
+import com.breeze.datatypes.BrzMessage;
 import com.breeze.datatypes.BrzNode;
 import com.breeze.dbmodels.DBBrzMessage;
 import com.breeze.dbmodels.DBBrzPreference;
 
 import org.json.JSONArray;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DatabaseHandler extends SQLiteOpenHelper {
@@ -24,13 +28,40 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "breezetables";
     private static final int DATABASE_VERSION = 1;
 
-    private static final String NODE_TABLE = "Nodes";
-    private static final String CHAT_TABLE = "Chats";
+    private static final String[] tableNames = new String[]{
+            "BrzChat",
+            "BrzMessage",
+            "BrzNode"
+    };
 
-    private static final String CONTACTS_TABLE_NAME = "Contacts";
-    private static final String MESSAGES_TABLE_NAME = "DBBrzMessage";
-    private static final String PREFERENCES_TABLE_NAME = "Preferences";
+    private static final String BRZCHAT_TABLE_NAME = "BrzChat";
+    private static final String BRZMESSAGE_TABLE_NAME = "BrzMessage";
+    private static final String BRZNODE_TABLE_NAME = "BrzNode";
 
+    private static final String INIT_BRZCHAT_TABLE = "CREATE TABLE IF NOT EXISTS BrzChat ('id' TEXT PRIMARY KEY, " +
+            "'name' TEXT NOT NULL, " +
+            "'publicKey' TEXT NOT NULL," +
+            "'privateKeyReference' TEXT NOT NULL," +
+            "'isGroup' BOOLEAN NOT NULL, " +
+            "'createdAt' DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+            "'updatedAt' DATETIME)";
+
+    private static final String INIT_BRZMESSAGE_TABLE = "CREATE TABLE IF NOT EXISTS BrzMessage ('id' INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "'from' TEXT NOT NULL, " +
+            "'body' TEXT NOT NULL, " +
+            "'chatId' TEXT NOT NULL, " +
+            "'isStatus' BOOLEAN NOT NULL, " +
+            "'createdAt' DATETIME DEFAULT CURRENT_TIMESTAMP, " +
+            "'updatedAt' DATETIME, " +
+            "'read' BOOLEAN NOT NULL DEFAULT 0," +
+            "FOREIGN KEY (chatId) REFERENCES BrzChat (id))";
+
+    private static final String INIT_BRZNODE_TABLE = "CREATE TABLE IF NOT EXISTS BrzNode ('id' TEXT PRIMARY KEY, " +
+            "'endpointId' TEXT NOT NULL, " +
+            "'publicKey' TEXT NOT NULL," +
+            "'name' TEXT NOT NULL, " +
+            "'alias' TEXT NOT NULL," +
+            "'metadataReference' TEXT NOT NULL)";
 
     public DatabaseHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -38,36 +69,18 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
-        final String nodeTableSQL = "CREATE TABLE IF NOT EXISTS " + NODE_TABLE + " ('id' TEXT PRIMARY KEY, 'endpointId' TEXT, 'publicKey' TEXT NOT NULL, 'name' TEXT NOT NULL, 'alias' TEXT NOT NULL)";
-        db.execSQL(nodeTableSQL);
-
-        final String chatTableSQL = "CREATE TABLE IF NOT EXISTS " + CHAT_TABLE + " ('id' TEXT PRIMARY KEY, 'name' TEXT NOT NULL, 'nodes' TEXT NOT NULL, 'isGroup' INTEGER NOT NULL)";
-        db.execSQL(chatTableSQL);
-
-
-        final String INIT_CONTACT_TABLE = "CREATE TABLE IF NOT EXISTS Contacts ('id' INTEGER PRIMARY KEY, 'name' TEXT NOT NULL, 'alias' TEXT NOT NULL, 'signature' TEXT NOT NULL, 'lasttalkedto' DATE NOT NULL, 'blocked' BOOLEAN NOT NULL, 'friend' BOOLEAN NOT NULL)";
-        db.execSQL(INIT_CONTACT_TABLE);
-        final String INIT_MESSAGE_TABLE = "CREATE TABLE IF NOT EXISTS Messages ('id' INTEGER PRIMARY KEY, 'from' INTEGER NOT NULL, 'body' TEXT NOT NULL, 'datetime' TEXT NOT NULL, 'encryption' TEXT DEFAULT NULL, FOREIGN KEY ('from') REFERENCES Contacts('id'))";
-        db.execSQL(INIT_MESSAGE_TABLE);
-        final String INIT_PREFS_TABLE = "CREATE TABLE IF NOT EXISTS Preferences ('id' INTEGER PRIMARY KEY, 'name' TEXT NOT NULL, 'setting' TEXT NOT NULL)";
-        db.execSQL(INIT_PREFS_TABLE);
-        Log.i("DatabaseInfo", "Table Creations succeeded for Contacts, Messages, Preferences");
+        db.execSQL(INIT_BRZCHAT_TABLE);
+        db.execSQL(INIT_BRZMESSAGE_TABLE);
+        db.execSQL(INIT_BRZNODE_TABLE);
+        Log.i("DatabaseInfo", "Table Creations succeeded for BrzChat, BrzMessage, BrzNode, and BrzProfile.");
     }
 
     @Override
     public void onUpgrade(@NonNull SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + NODE_TABLE);
-        db.execSQL("DROP TABLE IF EXISTS " + CHAT_TABLE);
-
-        //migration process if we want to upgrade in future.
-        //For now, it'll drop all tables and redo onCreate
-        final String DROP_CONTACT_TABLE = "DROP TABLE IF EXISTS Contact";
-        db.execSQL(DROP_CONTACT_TABLE);
-        final String DROP_MESSAGE_TABLE = "DROP TABLE IF EXISTS Messages";
-        db.execSQL(DROP_MESSAGE_TABLE);
-        final String DROP_PREFS_TABLE = "DROP TABLE IF EXISTS Preferences";
-        db.execSQL(DROP_PREFS_TABLE);
+        for(String tableName : DatabaseHandler.tableNames)
+        {
+            db.execSQL("DROP TABLE IF EXISTS" + tableName);
+        }
         this.onCreate(db);
     }
 
@@ -75,7 +88,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         String[] args = {node.id, node.endpointId, node.publicKey, node.name, node.alias};
         try {
-            db.execSQL("INSERT OR REPLACE INTO " + NODE_TABLE + " VALUES (?,?,?,?,?)", args);
+            db.execSQL("INSERT OR REPLACE INTO " + BRZNODE_TABLE_NAME + " VALUES (?,?,?,?,?)", args);
         } catch (Exception e) {
             Log.i("Bad SQL Error", "Error with SQL Syntax");
         }
@@ -86,7 +99,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         String[] args = {nodeId};
 
-        Cursor c = db.rawQuery("SELECT * FROM " + NODE_TABLE + " WHERE id = ?;", args);
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZNODE_TABLE_NAME + " WHERE id = ?;", args);
 
         if (c == null) {
             db.close();
@@ -117,7 +130,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         Object[] args = {chat.id, chat.name, new JSONArray(chat.nodes).toString(), chat.isGroup ? 1 : 0};
         try {
-            db.execSQL("INSERT OR REPLACE INTO " + CHAT_TABLE + " VALUES (?,?,?,?)", args);
+            db.execSQL("INSERT OR REPLACE INTO " + BRZCHAT_TABLE_NAME + " VALUES (?,?,?,?)", args);
         } catch (Exception e) {
             Log.i("Bad SQL Error", "Error with SQL Syntax");
         }
@@ -128,7 +141,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         Object[] args = {chatId};
         try {
-            db.execSQL("DELETE FROM " + CHAT_TABLE + " WHERE id = ?", args);
+            db.execSQL("DELETE FROM " + BRZCHAT_TABLE_NAME + " WHERE id = ?", args);
         } catch (Exception e) {
             Log.i("Bad SQL Error", "Error with SQL Syntax");
         }
@@ -138,7 +151,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
     public List<BrzChat> getAllChats() {
         SQLiteDatabase db = this.getReadableDatabase();
         String[] args = {};
-        Cursor c = db.rawQuery("SELECT * FROM " + CHAT_TABLE, args);
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZCHAT_TABLE_NAME, args);
 
         if (c == null) {
             db.close();
@@ -179,7 +192,7 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         String[] args = { chatId };
 
-        Cursor c = db.rawQuery("SELECT * FROM " + CHAT_TABLE + " WHERE id = ?;", args);
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZCHAT_TABLE_NAME + " WHERE id = ?;", args);
 
         if (c == null) {
             db.close();
@@ -207,71 +220,158 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         return n;
     }
 
-
-//    public void addContact(@NonNull DBBrzContact DBBrzContact) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues vals = new ContentValues();
-//        vals.put("id", DBBrzContact.getId());
-//        vals.put("name", DBBrzContact.getName());
-//        vals.put("alias", DBBrzContact.getAlias());
-//        vals.put("signature", DBBrzContact.getSignature());
-//        vals.put("lasttalkedto", DBBrzContact.getLastTalkedTo());
-//        vals.put("friend", DBBrzContact.isFriend());
-//        vals.put("blocked", DBBrzContact.isBlocked());
-//        db.insert(CONTACTS_TABLE_NAME, null, vals);
-//        db.close();
-//    }
-
-    public void addMessage(@NonNull DBBrzMessage message) {
+    public void addMessage(@NonNull BrzMessage message) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues vals = new ContentValues();
-        vals.put("id", message.getId());
-        vals.put("body", message.getBody());
-        vals.put("datetime", message.getDatetime());
-        vals.put("encryption", message.getEncryption());
-        db.insert(MESSAGES_TABLE_NAME, null, vals);
+        vals.put("from", message.from);
+        vals.put("body", message.body);
+        vals.put("isStatus", message.isStatus);
+        vals.put("chatId", message.chatId);
+        db.insert(BRZMESSAGE_TABLE_NAME, null, vals);
         db.close();
     }
-
-    public void addPreference(@NonNull DBBrzPreference preference) {
+    public void deleteMessage(@NonNull int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues vals = new ContentValues();
-        vals.put("id", preference.getId());
-        vals.put("name", preference.getName());
-        vals.put("setting", preference.getSetting());
-        db.insert(PREFERENCES_TABLE_NAME, null, vals);
+        db.delete(BRZMESSAGE_TABLE_NAME, "id = ?", new String[]{String.valueOf(id)});
         db.close();
     }
-
-//    public void deleteOneContact(@NonNull DBBrzContact contact) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        db.delete(CONTACTS_TABLE_NAME, "id = ?", new String[]{String.valueOf(contact.getId())});
-//        db.close();
-//    }
-
-    public void deleteOneMessage(@NonNull DBBrzMessage message) {
+    public BrzMessage getMessage(@NonNull int id){
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(MESSAGES_TABLE_NAME, "id = ?", new String[]{String.valueOf(message.getId())});
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZMESSAGE_TABLE_NAME + " WHERE id = ?;", new String[]{String.valueOf(id)});
+        if (c == null) {
+            db.close();
+            return null;
+        } else if (c.getCount() < 1) {
+            c.close();
+            db.close();
+            return null;
+        }
+        BrzMessage message = new BrzMessage();
+        c.moveToFirst();
+        message.from = c.getString(c.getColumnIndex("from"));
+        message.body = c.getString(c.getColumnIndex("body"));
+        message.chatId = c.getString(c.getColumnIndex("chatId"));
+        message.isStatus = c.getString(c.getColumnIndex("isStatus")).equals("1");
+        long milliseconds = 0;
+        SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+        try {
+            Date d = f.parse(c.getString(c.getColumnIndex("createdAt")));
+            milliseconds = d.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        message.datestamp = milliseconds;
+        c.close();
         db.close();
+        return message;
     }
 
-    public void deleteOnePreference(@NonNull DBBrzPreference preference) {
+    public List<BrzMessage> getInbox(@NonNull int currentUserId){
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(PREFERENCES_TABLE_NAME, "id = ?", new String[]{String.valueOf(preference.getId())});
-        db.close();
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZMESSAGE_TABLE_NAME + " WHERE 'from' != ?;", new String[]{String.valueOf(currentUserId)});
+        if (c == null) {
+            db.close();
+            return null;
+        } else if (c.getCount() < 1) {
+            c.close();
+            db.close();
+            return null;
+        }
+        ArrayList<BrzMessage> list = new ArrayList<>();
+        if (c.moveToFirst()) {
+            while (!c.isAfterLast()) {
+                BrzMessage message = new BrzMessage();
+                message.from = c.getString(c.getColumnIndex("from"));
+                message.body = c.getString(c.getColumnIndex("body"));
+                message.chatId = c.getString(c.getColumnIndex("chatId"));
+                message.isStatus = c.getString(c.getColumnIndex("isStatus")).equals("1");
+                long milliseconds = 0;
+                SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+                try {
+                    Date d = f.parse(c.getString(c.getColumnIndex("createdAt")));
+                    milliseconds = d.getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                message.datestamp = milliseconds;
+                list.add(message);
+                c.moveToNext();
+            }
+        }
+        return list;
     }
 
-//    public int updateContact(DBBrzContact contact) {
-//        SQLiteDatabase db = this.getWritableDatabase();
-//        ContentValues cv = new ContentValues();
-//        return -1;
-//    }
-
-    public int updateMessage(DBBrzMessage message) {
-        return -1;
+    public List<BrzMessage> getSentMessages(@NonNull int currentUserId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZMESSAGE_TABLE_NAME + " WHERE 'from' = ?;", new String[]{String.valueOf(currentUserId)});
+        if (c == null) {
+            db.close();
+            return null;
+        } else if (c.getCount() < 1) {
+            c.close();
+            db.close();
+            return null;
+        }
+        ArrayList<BrzMessage> list = new ArrayList<>();
+        if (c.moveToFirst()) {
+            while (!c.isAfterLast()) {
+                BrzMessage message = new BrzMessage();
+                message.from = c.getString(c.getColumnIndex("from"));
+                message.body = c.getString(c.getColumnIndex("body"));
+                message.chatId = c.getString(c.getColumnIndex("chatId"));
+                message.isStatus = c.getString(c.getColumnIndex("isStatus")).equals("1");
+                long milliseconds = 0;
+                SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+                try {
+                    Date d = f.parse(c.getString(c.getColumnIndex("createdAt")));
+                    milliseconds = d.getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                message.datestamp = milliseconds;
+                list.add(message);
+                c.moveToNext();
+            }
+        }
+        return list;
     }
 
-    public int updatePreference(DBBrzPreference preference) {
-        return -1;
+    public List<BrzMessage> getChatMessages(@NonNull int chatId){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor c = db.rawQuery("SELECT * FROM " + BRZMESSAGE_TABLE_NAME + " WHERE 'chatId' = ?;", new String[]{String.valueOf(chatId)});
+        if (c == null) {
+            db.close();
+            return null;
+        } else if (c.getCount() < 1) {
+            c.close();
+            db.close();
+            return null;
+        }
+        ArrayList<BrzMessage> list = new ArrayList<>();
+        if (c.moveToFirst()) {
+            while (!c.isAfterLast()) {
+                BrzMessage message = new BrzMessage();
+                message.from = c.getString(c.getColumnIndex("from"));
+                message.body = c.getString(c.getColumnIndex("body"));
+                message.chatId = c.getString(c.getColumnIndex("chatId"));
+                message.isStatus = c.getString(c.getColumnIndex("isStatus")).equals("1");
+                long milliseconds = 0;
+                SimpleDateFormat f = new SimpleDateFormat("dd-MMM-yyyy");
+                try {
+                    Date d = f.parse(c.getString(c.getColumnIndex("createdAt")));
+                    milliseconds = d.getTime();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+                message.datestamp = milliseconds;
+                list.add(message);
+                c.moveToNext();
+            }
+        }
+        return list;
     }
+
+
+
+
 }
