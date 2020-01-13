61c4dd4a383e500fb16d617092a097fd7160635a
jake stevens <jakestevens081@gmail.com>
2020-01-12 21:21:34 -0800

    delete sends message that they left the chat

--- app/src/main/java/com/breeze/router/BrzPayloadBuffer.java
+++ app/src/main/java/com/breeze/router/BrzPayloadBuffer.java
@@ -1,8 +1,17 @@
 package com.breeze.router;
 
+import android.content.Context;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.media.audiofx.AcousticEchoCanceler;
+import android.media.audiofx.NoiseSuppressor;
+
 import com.google.android.gms.nearby.connection.Payload;
 
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayInputStream;
+import java.io.DataOutputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -14,6 +23,27 @@ import java.util.function.Consumer;
 
 public class BrzPayloadBuffer {
 
+
+    public Payload sendStreamFromMic(){
+        int minBufferSize = AudioRecord.getMinBufferSize(44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+        AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, 44100, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, minBufferSize * 4);
+
+            if(NoiseSuppressor.isAvailable()){
+                NoiseSuppressor.create(recorder.getAudioSessionId()).setEnabled(true);
+            }
+            if(AcousticEchoCanceler.isAvailable()){
+                AcousticEchoCanceler.create(recorder.getAudioSessionId()).setEnabled(true);
+            }
+
+        byte[] tempBuffer = new byte[minBufferSize];
+        recorder.startRecording();
+        long currentTime = System.currentTimeMillis();
+        Payload p = Payload.fromStream(new ByteArrayInputStream(tempBuffer));
+        while(System.currentTimeMillis() < currentTime + 10000){
+
+        }
+        return null;
+    }
 
     public Payload getStreamPayload(String str) {
         return Payload.fromStream(new ByteArrayInputStream(str.getBytes()));
