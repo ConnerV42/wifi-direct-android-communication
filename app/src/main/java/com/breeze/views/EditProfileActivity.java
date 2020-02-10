4008f1671e07475c44f8f386b2def54bd3b06fb8
pavel chernichenko <pavelchernichenko@ymail.com>
2020-02-09 16:35:51 -0800

    More UI stuff for editing profile page

--- app/src/main/java/com/breeze/views/EditProfileActivity.java
+++ app/src/main/java/com/breeze/views/EditProfileActivity.java
@@ -1,10 +1,24 @@
 package com.breeze.views;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import androidx.annotation.Nullable;
+import android.graphics.Color;
 
+import com.breeze.R;
+import com.breeze.application.BreezeAPI;
+import com.breeze.datatypes.BrzNode;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
@@ -13,15 +27,23 @@ import androidx.fragment.app.Fragment;
 
 import com.breeze.R;
 import com.breeze.application.BreezeAPI;
+import com.breeze.datatypes.BrzNode;
 import com.breeze.graph.BrzGraph;
 
 public class EditProfileActivity extends Fragment
 {
+
+    private static final int READ_REQUEST_CODE = 42;
+    private BrzNode node = new BrzNode();
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+
+
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
@@ -31,6 +53,20 @@ public class EditProfileActivity extends Fragment
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        BreezeAPI api = BreezeAPI.getInstance();
+        BrzGraph graph = api.getGraph();
+        ImageView profileImage = getView().findViewById(R.id.profile_image_edit);
+
+
+        Button editPicture = getView().findViewById(R.id.edit_image_button);
+        //editPicture.setBackgroundColor(Color.RED);
+        editPicture.setOnClickListener(e -> {
+            profileImage.setImageBitmap(api.storage.getProfileImage(api.storage.PROFILE_DIR, "default"));
+            Intent intent = new Intent(Intent.ACTION_PICK,
+                    android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, READ_REQUEST_CODE);
+        });
 
 
     }
