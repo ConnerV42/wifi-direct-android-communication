f7b6d3b9bf67589395dae4e50d5f59c0960d60bf
pavel chernichenko <pavelchernichenko@ymail.com>
2020-02-10 22:03:13 -0800

    profile image and names transfer correctly now to editing page.

--- app/src/main/java/com/breeze/views/EditProfileActivity.java
+++ app/src/main/java/com/breeze/views/EditProfileActivity.java
@@ -14,8 +14,10 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import androidx.annotation.Nullable;
-import android.graphics.Color;
+import android.widget.TextView;
 
+import android.os.Bundle;
+import android.widget.Toast;
 import com.breeze.R;
 import com.breeze.application.BreezeAPI;
 import com.breeze.datatypes.BrzNode;
@@ -32,7 +34,7 @@ import com.breeze.graph.BrzGraph;
 
 public class EditProfileActivity extends Fragment
 {
-
+    View v;
     private static final int READ_REQUEST_CODE = 42;
     private BrzNode node = new BrzNode();
     @Override
@@ -55,18 +57,35 @@ public class EditProfileActivity extends Fragment
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         BreezeAPI api = BreezeAPI.getInstance();
-        BrzGraph graph = api.getGraph();
+        Toast.makeText(getActivity(), api.hostNode.name, Toast.LENGTH_LONG).show();
+
+
+        //set image
         ImageView profileImage = getView().findViewById(R.id.profile_image_edit);
+        //profileImage.setImageIcon();
+        profileImage.setImageBitmap(api.storage.getProfileImage(api.storage.PROFILE_DIR, api.hostNode.id));
 
+        //set username
+        TextView userName = getView().findViewById(R.id.user_name);
+        userName.setText(api.hostNode.name);
 
+        //set alias name
+        TextView aliasName = getView().findViewById(R.id.alias_name);
+        aliasName.setText(api.hostNode.alias);
+
+        //edit picture dialog pop up and save
         Button editPicture = getView().findViewById(R.id.edit_image_button);
-        //editPicture.setBackgroundColor(Color.RED);
+
         editPicture.setOnClickListener(e -> {
-            profileImage.setImageBitmap(api.storage.getProfileImage(api.storage.PROFILE_DIR, "default"));
             Intent intent = new Intent(Intent.ACTION_PICK,
                     android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
             startActivityForResult(intent, READ_REQUEST_CODE);
         });
+
+        //edit username dialog pop up and save
+
+
+        //edit alias dialog pop up and save
 
 
     }
